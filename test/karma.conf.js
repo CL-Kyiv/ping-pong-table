be3d3bd3ee87861b07b591343092039f2ca1cc9b
RathaR <ivan.pecharitsa@gmail.com>
2015-02-17 01:22:14 +0200

    install bootstrap-material-design

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -32,6 +32,8 @@ module.exports = function(config) {
       'bower_components/angular-mocks/angular-mocks.js',
       'bower_components/angular-aria/angular-aria.js',
       'bower_components/angular-material/angular-material.js',
+      'bower_components/bootstrap-material-design/dist/js/material.js',
+      'bower_components/bootstrap-material-design/dist/js/ripples.js',
       // endbower
       'app/scripts/**/*.js',
       'test/mock/**/*.js',
