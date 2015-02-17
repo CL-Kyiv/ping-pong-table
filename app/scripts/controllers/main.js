ad82f82a11ac417c2ebd87a44d101ab552b2eadf
ivanp <ivanp@compatibl.com>
2015-02-17 20:02:49 +0200

    try controller as syntax

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,7 @@
  * Controller of the pingPongTableApp
  */
 angular.module('pingPongTableApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function () {
+    this.player1 = 'IvanP';
+    this.player2 = 'IgorP';
   });
