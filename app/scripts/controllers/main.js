169c9374a861169038378361d9c5320fd3453a74
ivanp <ivanp@compatibl.com>
2015-02-17 20:57:09 +0200

    added some functionality

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,15 +12,37 @@ angular.module('pingPongTableApp')
     this.clearAll = function () {
       this.player1 = this.player2 = "noname";
       this.rank1 = this.rank2 = 0;
-    }
+    };
     this.clearAll();
-    this.incrementRank = function(player, value) {
-      if(player === 'player1') {
-        this.rank1 += value;
-      } else if(player === 'player2') {
-        this.rank2 += value;
+    this.save = function (player) {
+      var playerState = {};
+      playerState.name = player === 'player1' ? this.player1 : this.player2;
+      playerState.rank = player === 'player1' ? this.rank1 : this.rank2;
+      playerState = JSON.stringify(playerState);
+      localStorage.setItem(player, playerState);
+    }
+    this.load = function (player) {
+      var loadedState = localStorage.getItem(player);
+      loadedState = JSON.parse(loadedState);
+      if (player === 'player1') {
+        this.player1 = loadedState.name;
+        this.rank1 = loadedState.rank;
       } else {
-        throw new Error('Such player does\'t exist ' + player);
+        this.player2 = loadedState.name;
+        this.rank2 = loadedState.rank;
+      }
+    }
+    this.hasSaved = function (player) {
+      return _.indexOf(_.keys(localStorage), player) > -1;
+    }
+    this.clearLocalStorage = function() {
+      localStorage.clear();
+    }
+    this.incrementRank = function (player, value) {
+      if (player === 'player1') {
+        this.rank1 += value;
+      } else {
+        this.rank2 += value;
       }
     }
   });
