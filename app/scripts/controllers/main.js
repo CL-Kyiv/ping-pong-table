3e78cd1be8598cf3f64ad2a1bd1952d92eae8b8e
ivanp <ivanp@compatibl.com>
2015-02-17 20:33:22 +0200

    added simple logic

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,6 +9,18 @@
  */
 angular.module('pingPongTableApp')
   .controller('MainCtrl', function () {
-    this.player1 = 'IvanP';
-    this.player2 = 'IgorP';
+    this.clearAll = function () {
+      this.player1 = this.player2 = "noname";
+      this.rank1 = this.rank2 = 0;
+    }
+    this.clearAll();
+    this.incrementRank = function(player, value) {
+      if(player === 'player1') {
+        this.rank1 += value;
+      } else if(player === 'player2') {
+        this.rank2 += value;
+      } else {
+        throw new Error('Such player does\'t exist ' + player);
+      }
+    }
   });
